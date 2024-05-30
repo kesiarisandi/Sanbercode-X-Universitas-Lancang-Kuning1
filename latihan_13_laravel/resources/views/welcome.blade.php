b9322cd9cd042230a39ca838e4fa749de294e5ee
kesi ari sandi <kesiarisandi4@gmail.com>
2024-05-30 19:56:51 +0700

    tugas 13

---
+++ latihan_13_laravel/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+    @extends('layouts.master')
+    @section('title')
+    selamat datang!
+    @endsection
+    @section('content')
+    <h1>SELAMAT DATANG <?php echo $fname; ?> <?php echo $lname; ?></h1>
+    <h2>Terima kasih telah bergabung di SanberBook.Social Media kita bersama! </h2>
+@endsection
